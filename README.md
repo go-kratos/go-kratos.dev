2b86baea06f08470a64becc099a42867d2b9893d
windfarer <windfarer@gmail.com>
2020-12-18 00:02:06 +0800

    init

---
+++ README.md
@@ -0,0 +1,8 @@
+# Website
+
+[go-kratos.dev](https://kratos-go.dev) website.
+
+
+# 贡献文档
+所有文档在`docs`目录中，使用Markdown格式撰写，欢迎提交Pull Request.
+`master`分支的文档将自动构建并部署
---
+++ docusaurus.config.js
@@ -0,0 +1,101 @@
+module.exports = {
+  title: 'Kratos',
+  tagline: '轻量级的Go微服务框架',
+  url: 'https://go-kratos.dev',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'go-kratos',
+  projectName: 'kratos',
+  themeConfig: {
+    navbar: {
+      title: 'Kratos',
+      // logo: {
+      //   alt: 'Kratos Logo',
+      //   src: 'img/logo.svg',
+      // },
+      items: [
+        {
+          to: 'docs/',
+          activeBasePath: 'docs',
+          label: 'Docs',
+          position: 'left',
+        },
+        {
+          href: 'https://pkg.go.dev/github.com/go-kratos/kratos/',
+          label: 'API',
+          position: 'left',
+        },
+        {to: 'blog', label: 'Blog', position: 'right'},
+        {
+          href: 'https://github.com/go-kratos/kratos',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'Style Guide',
+              to: 'docs/',
+            },
+            {
+              label: 'Second Doc',
+              to: 'docs/doc2/',
+            },
+          ],
+        },
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'Discord',
+              href: 'https://discord.gg/BWzJsUJ',
+            },
+          ],
+        },
+        {
+          title: 'More',
+          items: [
+            {
+              label: 'Blog',
+              to: 'blog',
+            },
+            {
+              label: 'GitHub',
+              href: 'https://github.com/go-kratos/kratos',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} go-kratos. Built with Docusaurus.`,
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/go-kratos/go-kratos.dev/edit/master/website/',
+        },
+        blog: {
+          showReadingTime: true,
+          editUrl:
+            'https://github.com/go-kratos/go-kratos.dev/edit/master/website/blog/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+};
