8036ee1f15ceffedf7d64cfc50fd7255b82ff952
Windfarer <windfarer@gmail.com>
2021-09-30 21:16:30 +0800

    patching width, I don't know why, but it works

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,13 @@ module.exports = {
     purge: ['./src/**/*.html', './src/**/*.js', './src/**/*.tsx'],
     darkMode: false, // or 'media' or 'class'
     theme: {
+        screens: {
+            'sm': '640px',
+            'md': '768px',
+            'lg': '1024px',
+            'xl': '1280px',
+            '2xl': '1536px',
+        },
         container: {
             center: true,
         },
