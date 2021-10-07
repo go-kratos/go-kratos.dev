2fafd41c6eecf1a897728bee80795ed930945bb1
Windfarer <windfarer@gmail.com>
2021-10-07 16:15:43 +0800

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,7 +12,7 @@ module.exports = {
     colorMode: {
       defaultMode: 'light',
       disableSwitch: true,
-      respectPrefersColorScheme: "false",
+      respectPrefersColorScheme: false,
     },
     hideableSidebar: true,
     navbar: {
